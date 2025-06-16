67acc04c569a2f81e9accc1d01f87d296eb07646
Frammco19 <fa141359@gmail.com>
2025-06-16 00:39:49 -0300

    Dashboard conectando Backend

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import { books as BOOK_DATA } from "../../data/books"
 
@@ -7,19 +7,54 @@ import NewBook from "../newBook/NewBook"
 import { Button, Col, Row } from "react-bootstrap";
 
 const Dashboard = ({ onLogout }) => {
-    const [books, setBooks] = useState(BOOK_DATA);
+    const [books, setBooks] = useState([]);
 
-    const handleAddBook = (newBook) => {
-        const newBookWithId = {
-            id: books[books.length - 1].id + 1,
-            ...newBook,
-        };
-        setBooks(prevBooks => [newBookWithId, ...prevBooks])
+    useEffect(() => {
+    console.log("Dashboard")
+
+    fetch("http://localhost:8081/books")
+        .then(res => res.json())
+        .then(data => {
+          setBooks(data.map(book => ({
+            id: book.id,
+            title: book.title,
+            author: book.author.name,
+            pageCount: 100,
+            rating: 5,
+            imageUrl: null,
+            available: true
+          })))
+        })
+        .catch(err => console.log(err))
+  }, [])
+  
+
+   const handleAddBook = (newBook) => {
+     const newBookWithId = {
+       id: books[books.length - 1].id + 1,
+       ...newBook,
+     };
+     fetch("http://localhost:8081/books", {
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `eyJhbGciOiJIUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICI2MTFkNGU4MS0zZDBjLTRmODgtOTcxZS0yYThjYjAwNGEwM2UifQ.eyJleHAiOjE3NTAwNDU4MzksImlhdCI6MTc1MDA0NDAzOSwianRpIjoiZDJjNjIzY2ItMWU4OC00YzYxLThlNDktZGU4OTlmMTUwMTdiIiwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo4MDgwL3JlYWxtcy9teXJlYWxtIiwiYXVkIjoiaHR0cDovL2xvY2FsaG9zdDo4MDgwL3JlYWxtcy9teXJlYWxtIiwic3ViIjoiNGNjOGU3ZGMtNWQ5Mi00YTYwLThhMjAtNTRmYzYxNzJiYzUxIiwidHlwIjoiUmVmcmVzaCIsImF6cCI6InJlYWxtdGVzdCIsInNlc3Npb25fc3RhdGUiOiJiNjA4YTg2OC03YzY5LTQwNjktYWUzNS0yYmMxYjgzYzhjZTYiLCJzY29wZSI6InByb2ZpbGUgZW1haWwiLCJzaWQiOiJiNjA4YTg2OC03YzY5LTQwNjktYWUzNS0yYmMxYjgzYzhjZTYifQ.puKT7CK0NKdIsgvdHLtbJrJe6Y2sPXpY53baT7j8-HE`
+      },
+      method: "POST",
+      body: JSON.stringify({
+        title: newBook.title
+      })
+     })
+     .then(res => res.json())
+     .then(() => {
+      setBooks(prevBooks => [newBookWithId, ...prevBooks])
+     })
+     .catch(err => console.log(err))
     }
 
     const handleDeleteBook = (id) => {
         setBooks(prevBooks => prevBooks.filter(book => book.id !== id))
     }
+
     return (
         <>
             <Row className="w-100">
