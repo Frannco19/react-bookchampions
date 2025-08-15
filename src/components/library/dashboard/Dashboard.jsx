deb7715424492174ec328d095d4eb4a976bf957c
Frammco19 <fa141359@gmail.com>
2025-08-15 17:29:40 -0300

    Dashboard fix

--- src/components/library/dashboard/Dashboard.jsx
+++ src/components/library/dashboard/Dashboard.jsx
@@ -24,7 +24,7 @@ const Dashboard = ({ onLogout }) => {
         addBook(newBook,
             (response) => {
                 console.log(response)
-                fetch(`${import.meta.env.VITE_BASE_SERVER_URL}/api/book`)
+                fetch(`${import.meta.env.VITE_BASE_SERVER_URL}/api/Book`)
                     .then(res => res.json())
                     .then(data => {
                         setBooks([...data.result])
@@ -55,16 +55,19 @@ const Dashboard = ({ onLogout }) => {
 
     return (
         <>
-            <Row className="w-100">
+            <Row className="w-100 my-3">
                 <Col />
-                <Col md={3} className="d-flex justify-content-end m-3">
+                <Col md={3} className="d-flex justify-content-end ">
+                    <Button className="me-3" variant="success" onClick={handleNavigateAddBook}>Agregar libro</Button>
                     <Button onClick={onLogout}>Cerrar sesión</Button>
                 </Col>
             </Row>
             <h1>Book Champions</h1>
             <p>¡Bienvenidos a book champions!</p>
-            <NewBook onBookAdded={handleAddBook} />
-            <Books books={books} onDeleteBook={handleDeleteBook} />
+            <Routes>
+                <Route index element={<Books books={books} onDeleteBook={handleDeleteBook} />} />
+                <Route path="/add-book" element={<NewBook onBookAdded={handleAddBook} />} />
+            </Routes>
         </>
     )
 }
